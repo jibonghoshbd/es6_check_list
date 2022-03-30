7d3c37b657ddd9f37b0d4c0d81581f55d3d249cb
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-30 16:15:24 +0600

    number of array to get map each number dividing by 5

---
+++ array-map.js
@@ -0,0 +1,9 @@
+// array of numbers then map to get each number multiply 5 
+
+const numbers = [10, 20, 5, 25, 17, 45, 47, 59];
+const multiply5 = numbers.map(number => number * 5);
+console.log(multiply5)
+
+// array of numbers then map to get each number dividing by 5 
+const dividing5 = numbers.map(number => number / 5);
+console.log(dividing5)
