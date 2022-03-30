041acd2c6a9675fb15a145fa9875d16485081003
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-30 15:40:51 +0600

    es6 check list

---
+++ let-const.js
@@ -0,0 +1,7 @@
+let fastName = "Jibon";
+let lastName = "Ghosh";
+const profession = "Student";
+const StudentId = 101;
+
+console.log("My name is" + ' ' + fastName + ' ' + lastName);
+console.log("I am a " + ' ' + profession + '. ' + "My Id is:" + ' ' + StudentId);
