6b3d9902a0df7261d17ff22666af2d27669500c0
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-30 15:55:23 +0600

    simple arrow function

---
+++ arrow-function.js
@@ -0,0 +1,7 @@
+const dividing5 = (number) => number / 5;
+console.log(dividing5(50))
+
+const multiply = (num1, num2) => num1 * num2;
+const multiplyNumber1 = 10;
+const multiplyNumber2 = 10;
+console.log(multiply(multiplyNumber1, multiplyNumber2))
