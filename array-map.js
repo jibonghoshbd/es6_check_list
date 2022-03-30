24ce928caed88fb38189a366da1fc19e84947212
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-30 16:21:40 +0600

    get odd and even number of array

--- array-map.js
+++ array-map.js
@@ -1,9 +1,16 @@
 // array of numbers then map to get each number multiply 5 
 
-const numbers = [10, 20, 5, 25, 17, 45, 47, 59];
+const numbers = [10, 20, 5, 25, 17, 45, 47, 59, 580, 40];
 const multiply5 = numbers.map(number => number * 5);
-console.log(multiply5)
+// console.log(multiply5)
 
 // array of numbers then map to get each number dividing by 5 
 const dividing5 = numbers.map(number => number / 5);
-console.log(dividing5)
+// console.log(dividing5)
+
+//  numbers of array to get odd number 
+const getOddNumbers = numbers.filter(number => number % 2 === 1)
+console.log(getOddNumbers)
+
+const getEvenNumbers = numbers.filter(number => number % 2 != 1)
+console.log(getEvenNumbers)
