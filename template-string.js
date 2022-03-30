03ce14712c700a3c82f9433f1bba105e137ffda0
jibonghoshbd <jibonghosh1510@gmail.comm>
2022-03-30 15:45:19 +0600

    template string

---
+++ template-string.js
@@ -0,0 +1,7 @@
+let fastName = "Jibon";
+let lastName = "Ghosh";
+const profession = "Web Developer";
+const id = 101;
+
+const myInfo = `My name is: ${fastName} ${lastName}. I am a ${profession}. My id is: ${id}`;
+console.log(myInfo)
